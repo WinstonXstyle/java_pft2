0f6851cba70357acf176d5531512f2618e373d33
WinstonXstyle <88344908+WinstonXstyle@users.noreply.github.com>
2022-02-06 22:45:48 +0300

    добавлина площадь и периметор

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,22 +4,23 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         hello("world");
         hello("user");
-        double len = 5;
-        System.out.println("площадь квадрата со стороной " + len + "=" + area(len));
-        double a = 4;
-        double b = 6;
-        System.out.println("площадь треугольника со сторонами" + a + "и" + b + "=" + area(a,b));
+        Square s = new Square(5);
+        s.l = 5;
+        System.out.println("площадь квадрата со стороной " + s.l + "=" + area(s));
+        Rectangle r = new Rectangle(4,6);
+
+        System.out.println("площадь треугольника со сторонами " + r.a + "и" + r.b + "=" + area(r));
     }
     public static void hello(String somebode){
 
         System.out.println("hell " + somebode + "!");
     }
 
-    public static double area(double i){
-        return i * i;
+    public static double area(Square s){
+        return s.l * s.l;
     }
 
-    public static double area(double a, double b){
-        return a * b;
+    public static double area(Rectangle r){
+        return r.a * r.b;
     }
 }
